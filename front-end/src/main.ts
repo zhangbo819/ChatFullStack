14bb8469736b8828e41b8499bce501be648ba507
zhangbo01 <zhangbo01@meishubao.com>
2023-06-25 10:02:55 +0800

    feat(front): add vconsole

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -1,12 +1,15 @@
 import { createApp } from "vue";
 import ElementPlus from "element-plus";
-import 'element-plus/dist/index.css'
+import "element-plus/dist/index.css";
+import vConsole from 'vconsole'
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
 
+const vconsole = new vConsole();
+
 const AppBase = createApp(App);
 AppBase.use(router)
   .use(ElementPlus)
-  //   .use(vconsole as any)
+  .use(vconsole as any)
   .mount("#app");
